4c5819c9820927f5b27e5454de0e4c9a350684cb
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-07 18:13:15 +0900

    새 커스텀 hooks 생성 : useFieldValues

---
+++ src/hooks/useFieldValues.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+function useFieldValues(initialFieldValues) {
+  const [fieldValues, setFieldValues] = useState(initialFieldValues);
+
+  const clearFieldValues = () => setFieldValues(initialFieldValues);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFieldValues((prevFieldValues) => {
+      return {
+        ...prevFieldValues,
+        [name]: value,
+      };
+    });
+  };
+
+  return [fieldValues, handleChange, clearFieldValues, setFieldValues];
+}
+
+export default useFieldValues;
