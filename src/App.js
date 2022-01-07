02e0bfcaead72f04c1cb5b0f7e404df602e255ba
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-07 11:51:00 +0900

    react-router-dom을 활용한 페이징 처리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import ReviewList from 'pages/reviews/ReviewList';
+import Profile from 'pages/accounts/profile';
+import Login from 'pages/accounts/Login';
+import { Route, Routes } from 'react-router-dom';
+import TopNav from 'components/TopNav';
 
 function App() {
-  return <div>hello React</div>;
+  return (
+    <div>
+      <TopNav />
+      <Routes>
+        <Route path="/accounts/login/" element={<Login />} />
+        <Route path="/accounts/profile/" element={<Profile />} />
+        <Route path="/reviews/" element={<ReviewList />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/accounts/Login.jsx
@@ -0,0 +1,8 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+    </div>
+  );
+}
+export default Login;
