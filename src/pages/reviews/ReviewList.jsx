dafa69f9e992fd4b8e2ccda6c5e69310b3d386a9
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-07 14:50:56 +0900

    Review 컴포넌트를 활용하여 ReviewList 컴포넌트를 구성

--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -1,22 +1,22 @@
 import Axios from 'axios';
 import DebugStates from 'components/DebugStates';
+import Review from 'components/Review';
 import { useEffect, useState } from 'react';
 
 function PageReviewList() {
+  const [reviewList, setReviewList] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [reviewList, setReviewList] = useState([]);
 
   useEffect(() => {
     refetch();
   }, []);
 
   const refetch = () => {
-    setLoading(true);
     setError(null);
-
-    const url = 'http://localhost:8000/shop/api/reviews/';
-    // Promise 객체
+    setLoading(true);
+    const url = 'http://127.0.0.1:8000/shop/api/reviews/';
+    // Promise 객체 --> then, catch 지원, 체이닝 가능
     Axios.get(url)
       .then(({ data }) => {
         console.group('정상 응답');
@@ -28,30 +28,32 @@ function PageReviewList() {
         console.group('에러 응답');
         console.log(error);
         console.groupEnd();
+        setError(error);
       })
       .finally(() => {
         setLoading(false);
-      }); // 정상동작 후/ 에러동작후에도 무조건 적용되는 코드!
+      });
   };
 
   return (
     <div>
       <h2>Review List</h2>
-      {/* loading이 true시에 */}
-      {loading && <div>Loading ... </div>}
 
-      {/* loading이 false 시에  */}
-      {error && <div> 통신 중에 오류가 발생했습니다.</div>}
+      {loading && <div>Loading...</div>}
+      {error && <div>통신 중에 오류가 발생했습니다.</div>}
 
       <button
         onClick={() => refetch()}
-        className="bg-yellow-400 hover:bg-yellow-200"
+        className="bg-yellow-400 hover:bg-red-300"
       >
         새로고침
       </button>
 
+      {reviewList.map((review) => (
+        <Review key={review.id} review={review} />
+      ))}
+
       <hr />
-      <button></button>
       <DebugStates loading={loading} error={error} reviewList={reviewList} />
     </div>
   );
