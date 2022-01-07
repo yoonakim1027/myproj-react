7e751777e78869b7a91b8e90b778fa951bdffc21
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-07 16:36:00 +0900

    Review Delete API를 활용하여 리뷰 삭제를 구현

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,9 +1,29 @@
 import Rating from './Rating';
 
-function Review({ review }) {
+// handleDelete : 인자 없는 함수 -> 삭제 버튼 클릭 시에 호출 !
+// review : 보여질 리뷰 객체
+function Review({ review, handleEdit, handleDelete }) {
   const { content, score } = review;
+  // TODO : handleEdit/handleDelete 호출에 대한 방어적 코드를 작성
+  // 조건부 렌더링 필요
+  // {handleEdif &&} ~
+  //
   return (
     <div className="bg-yellow-100 border border-yellow-400 my-1 p-1">
+      <div>
+        <span
+          onClick={() => handleEdit()}
+          className="hover:text-blue-400 cursor-pointer mr-1"
+        >
+          수정
+        </span>
+        <span
+          className="hover:text-red-400 cursor-pointer"
+          onClick={() => handleDelete()}
+        >
+          삭제
+        </span>
+      </div>
       {content}
       <Rating score={score} />
     </div>
--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -35,6 +35,52 @@ function PageReviewList() {
       });
   };
 
+  // 삭제할 리뷰 객체를 인자로 받을 것!
+  // 필요한 것은 review의 id
+  // 콜론으로 이름을 바꿀 수 있음
+  // {원래이름 : 바꿀이름} -> 이렇게 바꾸고 값을 가져올 수 있음
+  const deleteReview = (deletingReview) => {
+    const { id: deletingReviewId } = deletingReview;
+    const url = `http://127.0.0.1:8000/shop/api/reviews/${deletingReviewId}/`;
+
+    // 삭제도 요청! -> 응답에 시간이 걸릴 수 있음 ==>
+    // Axios.에 다양한 요청을 보낼 수 있음!
+
+    //deleting요청이 들어가기 바로전에!!
+    setLoading(true);
+    setError(null);
+
+    // .then 시에 선택지들
+    // 1. 삭제된 항목만 상탯값에서 제거 2. 전체 새로고침(옛날 방식)
+
+    Axios.delete(url)
+      .then(() => {
+        console.log('삭제성공'); // then 호출만으로 성공을 알 수 있음
+        setReviewList((prevReviewList) => {
+          // prevReviewList 배열
+          // 매 순회마다 배열을 가져옴
+          return prevReviewList.filter((review) => {
+            // 중괄호!! 꼭 리턴
+            // arrow function + 중괄호 -> return 값 필요
+            // 문자열, 숫자열 끼리의 비교가 안전!
+            return review.id !== deletingReviewId;
+          }); // 삭제될 애들이 아닌애들만 남게 ~
+
+          // return 없이 ~
+          // setReviewList((prevReviewList)=>prevReviewList.filter(review)=> review.id ===deletingReviewId)
+        });
+      })
+      .catch((error) => {
+        setError(error); // 에러를 남기는 상탯값 -> 에러 날 때에는 할 게 딱히 없음
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    // 에러
+
+    console.log('Deleting', deletingReview);
+  };
+
   return (
     <div>
       <h2>Review List</h2>
@@ -50,7 +96,12 @@ function PageReviewList() {
       </button>
 
       {reviewList.map((review) => (
-        <Review key={review.id} review={review} />
+        <Review
+          key={review.id}
+          review={review}
+          handleDelete={() => deleteReview(review)}
+          // 삭제를 누르면, deleteReview를 호출하면서 리뷰 객체를 넘김
+        />
       ))}
 
       <hr />
