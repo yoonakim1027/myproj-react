a2222957a976e000dc0dedc8429cb8c8bc0e8b9b
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-07 15:42:38 +0900

    1. Review 컴포넌트에 별점 map에 key 지정 2. Alert 컴포넌트 개발 예시 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import TopNav from 'components/TopNav';
 
 import './App.css';
+import Components from 'pages/examples/Components';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/accounts/login/" element={<Login />} />
         <Route path="/accounts/profile/" element={<Profile />} />
         <Route path="/reviews/" element={<ReviewList />} />
+        <Route path="/examples/components/" element={<Components />} />
       </Routes>
     </div>
   );
---
+++ src/pages/examples/Components.jsx
@@ -0,0 +1,15 @@
+import Alert from 'components/Alert';
+
+function Components() {
+  return (
+    <div>
+      <h2 className="text-xl border-l-8 border-red-500 pl-1 mb-1">
+        Components
+      </h2>
+      <h3 className="text-lg border-l-4 border-red-500 pl-1 mb-1">Alerts</h3>
+      <Alert type="info" message="info 메시지 입니다" />
+      <Alert type="sucess" message="info 메시지 입니다" />
+    </div>
+  );
+}
+export default Components;
