a11a9cceb5a768b8fc349190c036cf049cc46918
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-10 11:38:58 +0900

    ReviewForm 컴포넌트에 loading 상탯값 추가

--- src/pages/reviews/ReviewForm.jsx
+++ src/pages/reviews/ReviewForm.jsx
@@ -3,9 +3,13 @@ import Axios from 'axios';
 import DebugStates from 'components/DebugStates';
 import ReviewForm from 'components/ReviewForm';
 import useFieldValues from 'hooks/useFieldValues';
+import { useState } from 'react';
 
 function PageReviewForm() {
   // 상탯값 정의. 훅 호출
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const navigate = useNavigate();
   const { reviewId } = useParams();
   const { fieldValues, handleFieldChange } = useFieldValues({
@@ -15,13 +19,17 @@ function PageReviewForm() {
 
   // 다양한 함수를 정의
   const saveReview = async () => {
+    setLoading(true); // 통신 중에는 참
+    setError(null);
     const url = 'http://localhost:8000/shop/api/reviews/';
     try {
       await Axios.post(url, fieldValues);
       navigate('/reviews/');
     } catch (e) {
+      setError(e);
       console.error(e);
     }
+    setLoading(false); // 통신 중에는 거짓
   };
 
   // 표현 by jsx
@@ -35,6 +43,7 @@ function PageReviewForm() {
         fieldValues={fieldValues}
         handleFieldChange={handleFieldChange}
         handleSubmit={saveReview}
+        loading={loading}
       />
       <DebugStates reviewId={reviewId} fieldValues={fieldValues} />
     </div>
