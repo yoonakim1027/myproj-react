60479536e98793f0b60c70b97e53a4bf8f7400e3
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-10 21:48:58 +0900

    PagePostForm, PagePostList 페이지 컴포넌트 생성

--- src/pages/blog/PagePostForm.jsx
+++ src/pages/blog/PagePostForm.jsx
@@ -1,2 +1,81 @@
-function PagePostForm() {}
+import { useNavigate, useParams } from 'react-router-dom';
+import Axios from 'axios';
+import DebugStates from 'components/DebugStates';
+import useFieldValues from 'hooks/useFieldValues';
+import { useEffect, useState } from 'react/cjs/react.development';
+import PostForm from 'components/blog/PostForm';
+
+function PagePostForm() {
+  // 상탯값 정의. 훅 호출
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+  const { postId } = useParams();
+  const { fieldValues, handleFieldChange, clearFieldValues, setFieldValues } =
+    useFieldValues({
+      title: '',
+      content: '',
+    });
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      setLoading(true);
+      setError(null);
+
+      const url = ` http://localhost:8000/blog/api/posts/${postId}/`;
+      try {
+        const response = await Axios.get(url);
+        setFieldValues(response.data);
+      } catch (error) {
+        setError(error);
+      }
+      setLoading(false);
+    };
+    if (postId) fetchPost();
+    else clearFieldValues();
+  }, [postId]);
+
+  // 다양한 함수를 정의
+  const savePost = async () => {
+    setLoading(true);
+    setError(null);
+
+    const url = !postId
+      ? 'http://localhost:8000/blog/api/posts/'
+      : `http://localhost:8000/blog/api/posts/${postId}/`;
+
+    try {
+      if (!postId) {
+        await Axios.post(url, fieldValues);
+      } else {
+        await Axios.patch(url, fieldValues);
+      }
+      navigate('/blog/');
+    } catch (eroor) {
+      setError(error);
+      console.error(error);
+    }
+
+    setLoading(false);
+  };
+
+  // 표현 by jsx
+  return (
+    <div>
+      <h2>
+        Post Form
+        {postId ? '수정' : '생성'}
+      </h2>
+      <PostForm
+        fieldValues={fieldValues}
+        handleFieldChange={handleFieldChange}
+        handleSubmit={savePost}
+        loading={loading}
+      />
+      <DebugStates postId={postId} fieldValues={fieldValues} />
+    </div>
+  );
+}
+
 export default PagePostForm;
--- src/pages/blog/PagePostList.jsx
+++ src/pages/blog/PagePostList.jsx
@@ -1,2 +1,132 @@
-function PagePostList() {}
+import Axios from 'axios';
+import DebugStates from 'components/DebugStates';
+import PostList from 'components/blog/PostList';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import PostForm from 'components/blog/PostForm';
+import PagePostDetail from './PagePostDetail';
+
+function PagePostList() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [postList, setPostList] = useState([]);
+
+  const [viewForm, setViewForm] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  const refetch = () => {
+    setLoading(true);
+    setError(null);
+
+    const url = 'http://localhost:8000/blog/api/posts/';
+    // Promise 객체
+    Axios.get(url)
+      .then(({ data }) => {
+        setPostList(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const deletePost = (deletingPost) => {
+    const { id: deletingPostId } = deletingPost;
+    const url = `http://localhost:8000/blog/api/posts/${deletingPostId}/`;
+
+    setLoading(true);
+    setError(null);
+
+    Axios.delete(url)
+      .then(() => {
+        console.log('삭제 성공');
+        // 선택지 #1) 삭제된 항목만 상탯값에서 제거
+        setPostList((prevPostList) =>
+          prevPostList.filter((post) => post.id !== deletingPostId),
+        );
+        // 선택지 #2) 전체를 새로고침
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const detailPost = (detailingPost) => {
+    const { id: detailingPostId } = detailingPost;
+    const url = `http://localhost:8000/blog/api/posts/${detailingPostId}/`;
+
+    setLoading(true);
+    setError(null);
+
+    Axios.get(url)
+      .then(() => {
+        console.log('불러오기 성공');
+        setPostList((prevPostList) =>
+          prevPostList.filter((post) => post.id === detailingPostId),
+        );
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <div>
+        <h2>Post List</h2>
+
+        {loading && <div>Loading ...</div>}
+        {error && <div>통신 중에 오류가 발생했습니다.</div>}
+
+        <div>
+          {!viewForm && (
+            <button
+              onClick={() => navigate('/blog/new/')}
+              className="bg-blue-400 hover:bg-slate-400"
+            >
+              새 포스팅
+            </button>
+          )}
+        </div>
+        <button
+          onClick={() => refetch()}
+          className="bg-yellow-400 hover:bg-red-400 mr-1"
+        >
+          새로고침
+        </button>
+
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          {postList.map((post) => (
+            <PostList
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              key={post.id}
+              post={post}
+              handleEdit={() => navigate(`/blog/${post.id}/edit/`)}
+              handleDelete={() => deletePost(post)}
+              onClick={() => navigate(`/blog/${post.id}/detail/`)}
+            />
+          ))}
+        </div>
+
+        <hr />
+        <DebugStates loading={loading} error={error} postList={postList} />
+      </div>
+    </>
+  );
+}
+
 export default PagePostList;
