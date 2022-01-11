e7baeca65cf754c098a9b7be14ebde23a6b610e1
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-11 13:23:13 +0900

    디지털 시계에 년/월/일/요일 정보를 추가

--- src/pages/examples/Clock.jsx
+++ src/pages/examples/Clock.jsx
@@ -7,25 +7,33 @@ function Clock() {
   const [date, setDate] = useState(new Date());
 
   useEffect(
-    // 이 함수는 현 컴포넌트가 mount 시에 호출됨
+    // 이 함수는 현 컴포넌트가 mount 시에 호출됩니다.
     () => {
       const interval = setInterval(() => {
         setDate(new Date());
       }, 1000);
-      // 리턴된 함수는 현 컴포넌트 unmount시에 호출됨
+      // 리턴된 함수는 현 컴포넌트 unmount 시에 호출됩니다.
       return () => {
         clearInterval(interval);
       };
     },
     [],
   );
+
   return (
     <div className="clock-wrapper">
       <h2>시계</h2>
-      <div className="clock">
-        <p className="date">{date.toLocaleDateString()}</p>
-        <p className="time">{date.toLocaleTimeString('en-us')}</p>
-        <p className="text">Powered by React.js</p>
+
+      <div class="clock">
+        <p class="date">
+          {date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}{' '}
+          {WEEKDAYS[date.getDay()]}
+        </p>
+        {/* <p class="time">{date.toLocaleTimeString('en-us')}</p> */}
+        <p class="time">
+          {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}
+        </p>
+        <p class="text">Powered by React.js</p>
       </div>
     </div>
   );
