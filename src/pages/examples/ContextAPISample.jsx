47da3f2fcc81008330c821c264322c99aad556d8
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-11 16:24:18 +0900

    dispatch(useReducer) 버전 ContextAPI 접근

--- src/pages/examples/ContextAPISample.jsx
+++ src/pages/examples/ContextAPISample.jsx
@@ -1,18 +1,23 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useReducer, useState } from 'react';
 
 // 인자 : context에서 다룰 값의 디폴트 값
-
 const MessageContext = createContext();
 
+// dispatch 버전
+function reducer(prevCount, action) {
+  const { type } = action;
+  if (type === 'PLUS') return prevCount + 1;
+  return prevCount;
+}
+
 function ContextApiSample() {
-  // 카운트의 초기값은 0
-  const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
+
   return (
     <div>
       <h2>ContextApiSample</h2>
-      <button onClick={() => setCount((prev) => prev + 1)}>1씩 증가</button>
-      <button onClick={() => setCount((prev) => prev - 1)}>1씩 감소</button>
-      <MessageContext.Provider value={count}>
+      <button onClick={() => dispatch({ type: 'PLUS' })}>1씩 증가</button>
+      <MessageContext.Provider value={{ count, dispatch }}>
         <Level1 />
       </MessageContext.Provider>
     </div>
@@ -41,25 +46,19 @@ function Level3() {
   return (
     <div>
       <h2>Level3</h2>
-      {/* 메시지를 소비하는 것!  */}
-      <MessageContext.Consumer>
-        {/* 이 사이에 쓰는 것이 children */}
-        {(count) => count}
-        {/* 함수인자로 message를 받고~ 그냥 message를 리턴 */}
-      </MessageContext.Consumer>
-      <level4 />
+      <MessageContext.Consumer>{({ count }) => count}</MessageContext.Consumer>
+      <Level4 />
     </div>
   );
 }
 
-// 함수 컴포넌트는 훅이 다 있다.
-// level3의 코드와 level4의 코드는 동일한 동작을 한다
 function Level4() {
-  const count = useContext(MessageContext);
+  const { count, dispatch } = useContext(MessageContext);
   return (
     <div>
       <h2>Level4</h2>
-      {count}
+      <div>{count}</div>
+      <button onClick={() => dispatch({ type: 'PLUS' })}>1씩 증가</button>
     </div>
   );
 }
