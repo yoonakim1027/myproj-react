0ded75302f4e55131ec4d1cd3ccc9bba0a433345
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-12 23:16:20 +0900

    added ErrorBoundary 컴포넌트

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ErrorBoundary from 'ErrorBoundary';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root'),
 );
