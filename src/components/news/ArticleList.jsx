b43e80383b7ad3c1508bdc8eae293fa8cc16afd2
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-13 13:28:53 +0900

    news : animation - transition 적용

--- src/components/news/ArticleList.jsx
+++ src/components/news/ArticleList.jsx
@@ -19,7 +19,10 @@ function ArticleList() {
       {articleList && (
         <div className="flex flex-wrap">
           {articleList.map((article) => (
-            <div key={article.id} className="w-full md:w-1/2 xl:w-1/3 px-4">
+            <div
+              key={article.id}
+              className="transition-transform hover:-translate-y-5 duration-300 w-full md:w-1/2 xl:w-1/3 px-4"
+            >
               <ArticleSummary article={article} />
             </div>
           ))}
